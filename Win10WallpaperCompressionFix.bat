bf094dda64f340767a6e9c3eabb882ecb96c6c4d
Viotoxin <132709865+Viotoxin@users.noreply.github.com>
2024-08-21 08:22:00 -0500

    Update Win10WallpaperCompressionFix.bat

--- Win10WallpaperCompressionFix.bat
+++ Win10WallpaperCompressionFix.bat
@@ -1,7 +1,11 @@
+:: @echo off just hides the command execution details from appearing in the console.
 @echo off
+  
 :: Set the JPEGImportQuality registry value to 100 (decimal) to prevent Desktop Wallpaper Compression in Windows 10
-
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v JPEGImportQuality /t REG_DWORD /d 100 /f
 
+:: Confirmation message for user
 echo JPEGImportQuality Registry key updated successfully. Restart & Reapply wallpaper and it should no longer be compressed.
+
+:: Leave terminal window open for user to read
 pause
